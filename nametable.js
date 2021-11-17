a8a756f9439980ebadf88476511cad2d10821162
ssocolow <ssocolow@github.com>
2021-11-16 22:10:32 -0500

    tried to start using react, maybe do demo but just want a table and stuff so maybe don't use it?

---
+++ nametable.js
@@ -0,0 +1,14 @@
+class NameTable extends React.Component {
+  render() {
+    return (
+      <div className="nametable">
+      <p>niw is a test</p>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+    document.getElementById('root')
+    );
